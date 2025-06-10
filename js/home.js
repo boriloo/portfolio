a87493607e18ad2e55567fd602dc9f3f27235d3a
Borilo537 <narutocamufladoxd@gmail.com>
2025-06-10 15:52:44 -0300

    modo economia e ajustes

--- js/home.js
+++ js/home.js
@@ -1,110 +1,126 @@
-
-const lastElement = document.querySelector('.bosta')
-const distance = lastElement.getBoundingClientRect().top + window.scrollY
-const realDistance = document.querySelector('.bosta').offsetHeight + distance
-const fakeScroll = document.querySelector('.fake-scroll')
-fakeScroll.style.height = `${realDistance + 200}px`
-const circle = document.querySelector('.cursor-ball');
-const sectionRow = document.querySelector('.sections')
-const topSection = document.querySelector('.top-section')
-let scrollValue = 0
+const lastElement = document.querySelector(".bosta");
+const distance = lastElement.getBoundingClientRect().top + window.scrollY;
+const realDistance = document.querySelector(".bosta").offsetHeight + distance;
+const fakeScroll = document.querySelector(".fake-scroll");
+fakeScroll.style.height = `${realDistance + 200}px`;
+const circle = document.querySelector(".cursor-ball");
+const sectionRow = document.querySelector(".sections");
+const topSection = document.querySelector(".top-section");
+const scrollContainer = document.querySelector(".scroll-container");
+let highGraph = true;
+let scrollValue = 0;
 
 let lastScroll = 0;
 
-window.addEventListener('scroll', () => {
-    targetY = window.scrollY;
-    const currentScroll = window.scrollY;
-    if (currentScroll > lastScroll) {
-        sectionRow.style.opacity = '0';
-        sectionRow.style.pointerEvents = 'none';
-        topSection.style.opacity = '0';
-        topSection.style.pointerEvents = 'none';
-    } else {
-        sectionRow.style.opacity = '1';
-        sectionRow.style.pointerEvents = 'all';
-        topSection.style.opacity = '1';
-        topSection.style.pointerEvents = 'all';
-    }
-    lastScroll = currentScroll;
+const graphIcon = document.querySelector(".graph-icon");
+graphIcon.addEventListener("click", () => {
+  if (highGraph) {
+    // ECONOMIA
+    circle.style.filter = `blur(0px)`;
+    scrollContainer.style.filter = `blur(0px)`;
+    graphIcon.classList.remove("bi-stars");
+    graphIcon.classList.add("bi-star-fill");
+  } else {
+    // GRÁFICO
+    graphIcon.classList.add("bi-stars");
+    graphIcon.classList.remove("bi-star-fill");
+  }
+  highGraph = !highGraph;
 });
 
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  if (currentScroll > lastScroll) {
+    sectionRow.style.opacity = "0";
+    sectionRow.style.pointerEvents = "none";
+    topSection.style.opacity = "0";
+    topSection.style.pointerEvents = "none";
+  } else {
+    sectionRow.style.opacity = "1";
+    sectionRow.style.pointerEvents = "all";
+    topSection.style.opacity = "1";
+    topSection.style.pointerEvents = "all";
+  }
+  lastScroll = currentScroll;
+});
 
 // LIGHT / DARK MODE
-let modo = localStorage.getItem('mode');
+let modo = localStorage.getItem("mode");
 let darkMode = true;
 if (modo) {
-    switch (modo) {
-        case 'dark':
-            break;
-        case 'light':
-            darkMode = false;
-            document.documentElement.classList.remove('dark');
-            break;
-    }
+  switch (modo) {
+    case "dark":
+      break;
+    case "light":
+      darkMode = false;
+      document.documentElement.classList.remove("dark");
+      break;
+  }
 }
-const modeIcon = document.querySelector('.mode-icon');
-const darkBall = document.querySelector('.dark-ball');
-const lightBall = document.querySelector('.light-ball');
+const modeIcon = document.querySelector(".mode-icon");
+const darkBall = document.querySelector(".dark-ball");
+const lightBall = document.querySelector(".light-ball");
 
 function toggleModeAnimation(showBall, hideBall, iconAdd, iconRemove) {
-    showBall.style.transform = 'scale(50)';
-    document.body.classList.add('locked');
-    showBall.style.left = '0';
-    showBall.style.bottom = '0';
-    showBall.style.right = 'auto';
-    showBall.style.top = 'auto';
-    hideBall.style.right = '0';
-    hideBall.style.top = '0';
-    hideBall.style.left = 'auto';
-    hideBall.style.bottom = 'auto';
+  showBall.style.transform = "scale(50)";
+  document.body.classList.add("locked");
+  showBall.style.left = "0";
+  showBall.style.bottom = "0";
+  showBall.style.right = "auto";
+  showBall.style.top = "auto";
+  hideBall.style.right = "0";
+  hideBall.style.top = "0";
+  hideBall.style.left = "auto";
+  hideBall.style.bottom = "auto";
+  setTimeout(() => {
+    showBall.style.transform = "scale(0)";
+  }, 600);
+  setTimeout(() => {
+    document.documentElement.classList.toggle("dark");
+    hideBall.style.transform = "scale(50)";
+    modeIcon.classList.add(iconAdd);
+    modeIcon.classList.remove(iconRemove);
+
     setTimeout(() => {
-        document.documentElement.classList.toggle('dark');
-        hideBall.style.transform = 'scale(50)';
-        showBall.style.transform = 'scale(0)';
-        modeIcon.classList.add(iconAdd);
-        modeIcon.classList.remove(iconRemove);
+      document.body.classList.remove("locked");
+      hideBall.style.opacity = "0";
+
+      setTimeout(() => {
+        hideBall.style.transform = "scale(0)";
 
         setTimeout(() => {
-            document.body.classList.remove('locked');
-            hideBall.style.opacity = '0';
-
-            setTimeout(() => {
-                hideBall.style.transform = 'scale(0)';
-
-                setTimeout(() => {
-                    hideBall.style.opacity = '1';
-                }, 500);
-            }, 300);
+          hideBall.style.opacity = "1";
         }, 500);
+      }, 300);
     }, 500);
+  }, 500);
 }
 
-modeIcon.addEventListener('click', () => {
-    if (darkMode) {
-        localStorage.setItem('mode', 'light');
-        toggleModeAnimation(darkBall, lightBall, 'bi-moon', 'bi-sun');
-    } else {
-        localStorage.setItem('mode', 'dark');
-        toggleModeAnimation(lightBall, darkBall, 'bi-sun', 'bi-moon');
-    }
+modeIcon.addEventListener("click", () => {
+  if (darkMode) {
+    localStorage.setItem("mode", "light");
+    toggleModeAnimation(darkBall, lightBall, "bi-moon", "bi-sun");
+  } else {
+    localStorage.setItem("mode", "dark");
+    toggleModeAnimation(lightBall, darkBall, "bi-sun", "bi-moon");
+  }
 
-    darkMode = !darkMode;
+  darkMode = !darkMode;
 });
 
-
-let langTextCounter = 0
-let langText = ''
-const langs = document.querySelectorAll('.lang');
-const bigLangText = document.querySelector('.big-lang-text');
+let langTextCounter = 0;
+let langText = "";
+const langs = document.querySelectorAll(".lang");
+const bigLangText = document.querySelector(".big-lang-text");
 langs.forEach((lang) => {
-    lang.addEventListener('mouseenter', () => {
-        langTextCounter = 60
-        langText = lang.innerHTML
-    });
-})
+  lang.addEventListener("mouseenter", () => {
+    langTextCounter = 60;
+    langText = lang.innerHTML;
+  });
+});
 
 // WHOLE PAGE LERP EFFECT
-const topText = document.querySelector('.top-text');
+const topText = document.querySelector(".top-text");
 
 let mouseX = 0;
 let mouseY = 0;
@@ -117,62 +133,61 @@ let lastY = 0;
 const inertia = 0.3;
 const drag = 0.9;
 
-document.addEventListener('mousemove', e => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
-    velocityX = (mouseX - lastX);
-    velocityY = (mouseY - lastY);
-
-    lastX = mouseX;
-    lastY = mouseY;
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  if (!highGraph) return;
+  velocityX = mouseX - lastX;
+  velocityY = mouseY - lastY;
+  lastX = mouseX;
+  lastY = mouseY;
 });
 
 let current = 0;
 const ease = 0.08;
 
 function animate(e) {
-    offsetX += velocityX;
-    offsetY += velocityY;
-    offsetX *= drag;
-    offsetY *= drag;
-    const finalX = mouseX + offsetX * inertia;
-    const finalY = mouseY + offsetY * inertia;
+  offsetX += velocityX;
+  offsetY += velocityY;
+  offsetX *= drag;
+  offsetY *= drag;
+  const finalX = mouseX + offsetX * inertia;
+  const finalY = mouseY + offsetY * inertia;
 
-    const target = window.scrollY;
-    const diff = target - current;
-    if (Math.abs(diff) < 0.1) {
-        current = target;
-    } else {
-        current += diff * ease;
-    }
+  const target = window.scrollY;
+  const diff = target - current;
+  if (Math.abs(diff) < 0.1) {
+    current = target;
+  } else {
+    current += diff * ease;
+  }
 
-    const scrollContainer = document.querySelector('.scroll-container');
-    scrollContainer.style.marginTop = `${-current}px`;
+  scrollContainer.style.marginTop = `${highGraph ? -current : -target}px`;
+  if (highGraph)
     scrollContainer.style.filter = `blur(${Math.abs(diff) * 0.02}px)`;
-    if (window.innerWidth > 770) {
-        topText.style.transform = `perspective(25vi) rotateY(${25 - current * 0.03}deg)`
-    } else {
-        topText.style.transform = `perspective(0) rotateY(0deg)`
-    }
-    circle.style.left = `${finalX}px`;
-    circle.style.top = `${finalY}px`;
+  if (window.innerWidth > 770) {
+    topText.style.transform = `perspective(25vi) rotateY(${
+      25 - current * 0.03
+    }deg)`;
+  } else {
+    topText.style.transform = `perspective(0) rotateY(0deg)`;
+  }
+  circle.style.left = `${highGraph ? finalX : mouseX}px`;
+  circle.style.top = `${highGraph ? finalY : mouseY}px`;
+  if (highGraph)
     circle.style.filter = `blur(${Math.abs(finalX - mouseX) * 0.08}px)`;
 
-    if (langTextCounter > 0) {
-        langTextCounter -= 1;
-        bigLangText.style.opacity = '0'
-        if (langTextCounter < 30) {
-            bigLangText.innerHTML = langText
-        }
-    } else {
-        bigLangText.style.opacity = '1'
+  if (langTextCounter > 0) {
+    langTextCounter -= 1;
+    bigLangText.style.opacity = "0";
+    if (langTextCounter < 30) {
+      bigLangText.innerHTML = langText;
     }
+  } else {
+    bigLangText.style.opacity = "1";
+  }
 
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 }
 
 animate();
-
-
-
--- js/sections.js
+++ js/sections.js
@@ -1,14 +1,14 @@
-const habilidade = document.getElementById('hab');
-const el = document.querySelector('.section-2');
+const habilidade = document.getElementById("hab");
+const el = document.querySelector(".section-2");
 
-habilidade.addEventListener('click', () => {
-    scrollToElement(el);
+habilidade.addEventListener("click", () => {
+  scrollToElement(el);
 });
 
 function scrollToElement(el, offset = 0) {
-    const y = el.getBoundingClientRect().top + window.scrollY + offset;
-    window.scrollTo({
-        top: y - 300,
-        behavior: 'smooth'
-    });
+  const y = el.getBoundingClientRect().top + window.scrollY + offset;
+  window.scrollTo({
+    top: y - y * 0.1,
+    behavior: "smooth",
+  });
 }
