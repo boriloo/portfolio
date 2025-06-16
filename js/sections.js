5c513fb73c93d8209024509bc5498b53181ae188
murilo <narutocamufladoxd@gmail.com>
2025-06-16 11:21:53 -0300

    updt

--- js/sections.js
+++ js/sections.js
@@ -1,14 +1,28 @@
 const habilidade = document.getElementById("hab");
-const el = document.querySelector(".section-2");
-
+const sectionHab = document.querySelector(".section-2");
 habilidade.addEventListener("click", () => {
-  scrollToElement(el);
+  scrollToElement(sectionHab);
 });
 
+const sobre = document.getElementById("sob");
+const sectionSob = document.querySelector(".section-about");
+sobre.addEventListener("click", () => {
+  scrollToElement(sectionSob);
+});
+
+const proj = document.getElementById("pro");
+const sectionPro = document.querySelector(".section-3");
+proj.addEventListener("click", () => {
+  scrollToElement(sectionPro);
+});
+
+
 function scrollToElement(el, offset = 0) {
-  const y = el.getBoundingClientRect().top + window.scrollY + offset;
+  const elementTop = window.scrollY + el.getBoundingClientRect().top;
+  const targetY = elementTop - (window.innerHeight / 2) + (el.offsetHeight / 2) + offset;
+
   window.scrollTo({
-    top: y - y * 0.1,
+    top: targetY,
     behavior: "smooth",
   });
 }
